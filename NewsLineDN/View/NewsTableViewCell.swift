ba684d05bd98017e86518ec563df7c4ff3e6f8ce
Ivan Nikitin <nqqaqq@gmail.com>
2020-01-22 17:15:05 +0700

    navigation without storyboard

---
+++ NewsLineDN/View/NewsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  NewsTableViewCell.swift
+//  NewsLineDN
+//
+//  Created by Ivan Nikitin on 22.01.2020.
+//  Copyright © 2020 Ivan Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var title: UILabel!
+    @IBOutlet var newsImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NewsLineDN/View/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  NewsLineDN
+//
+//  Created by Ivan Nikitin on 22.01.2020.
+//  Copyright © 2020 Ivan Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    var newsTitle: String?
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let newsTitle = newsTitle {
+            title = newsTitle
+            label.text = newsTitle
+        }
+    }
+}
